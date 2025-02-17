60850204faf881f9f5b69e97061fe0832def3aff
liqiang <liqiangeastsun@163.com>
2025-02-17 16:44:47 +0800

    modify

--- shell/testShellParams.sh
+++ shell/testShellParams.sh
@@ -3,7 +3,7 @@
 
 #!/bin/bash
 
-echo "this is testShell.sh"
+echo "this is testShellParams.sh"
 
 # 输出 Jenkins 中参数
 echo "Workspace=${WORKSPACE}"
@@ -12,6 +12,8 @@ echo "BRANCH_NAME=${BRANCH_NAME}"
 echo "ABC=${ABC}"
 echo "Type=${Type}"
 
+# Pipeline 通过 withEnv(["MY_PARAM_ABC=${ABC}", "MY_PARAM_Type=${Type}"]) 传递参数
+# 在这里可以直接获取
 echo "MY_PARAM_ABC=${MY_PARAM_ABC}"
 echo "MY_PARAM_Type=${MY_PARAM_Type}"
 
