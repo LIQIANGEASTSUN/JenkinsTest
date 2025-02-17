2e29c252bfddebb250d2863905e21ff6394ada05
liqiang <liqiangeastsun@163.com>
2025-02-17 21:17:23 +0800

    modify

--- shell/testShellUnity.sh
+++ shell/testShellUnity.sh
@@ -5,7 +5,11 @@
 
 echo "this is testShellUnity.sh"
 
+echo "WORKSPACE=${WORKSPACE}"
 UNITY_PATH=/Applications/Unity/Hub/Editor/2022.3.26f1/Unity.app/Contents/MacOS/Unity
+
+PROJECT_PATH="${WORKSPACE}/TestProject1/Project"
+echo "PROJECT_PATH=${PROJECT_PATH}"
 
 # 设置环境变量
 export MY_PARAM="ttttttAAAAAA"
