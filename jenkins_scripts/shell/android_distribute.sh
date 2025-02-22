9120253252c325c491cd3e9e7053ac0c8f99c61d
liqiang <liqiangeastsun@163.com>
2025-02-22 16:36:37 +0800

    modify

--- jenkins_scripts/shell/android_distribute.sh
+++ jenkins_scripts/shell/android_distribute.sh
@@ -68,3 +68,29 @@ java -jar ${BUNDLE_TOOL_PATH} extract-apks --apks=${OUT_PUT_APKS_PATH_1} --outpu
 
 
 echo "this is android_distribute.sh end"
+
+
+# ProjectExportApk.ExportApkAndAAB 方法中生成 .apk 和 .aab 文件
+# Android 上线 Google Play 只需要将生成的 .aab 上传到 GooglePlay 即可
+
+# bundletool 命令的作用，官方文档 https://developer.android.com/tools/bundletool?hl=zh-cn
+# java -jar ${BUNDLE_TOOL_PATH} build-apks --bundle=${GOOGLE_PLAY_AAB_PATH} --output=${OUT_PUT_PATH} 
+# 作用是使用 bundletool 工具从 Android App Bundle（.aab 文件）生成 APK 集合
+# 并将生成的 APK 集合打包成一个名为“APK set archive”的文件，文件扩展名为 .apks
+# 这个命令会将指定的 .aab 文件转换为 APK 集合，并将结果输出到指定的路径（OUT_PUT_PATH）。
+
+
+# java -jar ${BUNDLE_TOOL_PATH} extract-apks --apks=${OUT_PUT_PATH} --output-dir=${OUT_APKS_PATH} --device-spec=${SAMSUNG_S9_PATH} 
+# 作用是从之前生成的 APK 集合（.apks 文件）中提取出针对特定设备配置的 APK
+# --device-spec 参数指定了一个 JSON 文件（SAMSUNG_S9_PATH），该文件描述了目标设备的特性（如屏幕大小、CPU 架构等），从而确保提取的 APK 适合该设备
+
+# 从上面两条 bundletool 命令的作用，我们了解到，只是用 .aab 生成 apk 组到 .apks，然后再通过 --device-spec 指定的 Json 配置文件
+# 从 .apks 文件中提取出满足特定设备的 apk
+# .aab 文件还是ProjectExportApk.ExportApkAndAAB 方法中生成的，没有做任何处理
+# 那么为什么要添加上面两条 bundletool 命令?
+# 这在开发和测试阶段是非常有用的，因为它允许开发者在本地测试和验证 APK 的功能，特别是在不同的设备配置下确保其在目标设备上能正常运行。
+# 对于上传到 Google Play 的生产版本，这些步骤可能不是必须的，但常常在 CI/CD 流程中用于验证构建的有效性。
+# 这些命令在比较典型的开发工作流中是有用的，尤其是在测试和验证阶段，当开发者需要验证构建后的 APK 是否如预期那样工作时
+
+
+
